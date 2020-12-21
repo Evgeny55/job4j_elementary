c129fa18d21e8665907e0d09baa4448bc8f13545
3.1.Загрузка файлов в gidhub. [#243562] <e.urin1983@gmail.com>
2020-12-21 11:28:21 -0600

    3.1.Загрузка файлов в Gidhub. [#243563]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+import java.sql.SQLOutput;
+
+public class Multiple {
+    public static void main(String[] args) {
+//        1 * 2 = 2;
+//        1 * 3 = 3;
+//        1 * 4 = 4;
+//        1 * 5 = 5;
+//        1 * 6 = 6;
+//        1 * 7 = 7;
+//        1 * 8 = 8;
+//        1 * 9 = 9;
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
+        System.out.println("1 * 9 = 9");
+    }
+
+
+}
