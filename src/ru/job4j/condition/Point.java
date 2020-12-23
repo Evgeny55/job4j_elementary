bbb341f0044b40166991c5c5d06c694cc9edb6da
3.1.Загрузка файлов в gidhub. [#243562] <e.urin1983@gmail.com>
2020-12-23 11:30:16 -0600

    3.1.Загрузка файлов в Gidhub. [#243569]

---
+++ src/ru/job4j/condition/Point.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double rsl = x2 - x1;
+        return rsl;
+
+        double rsl = y2 - y1;
+        return rsl;
+
+    }
+
+    public static void main(String[] args) {
+        double result1 = Point.distance(3,4,5,6);
+        double result2 = Point.distance(3,4,5,6);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
