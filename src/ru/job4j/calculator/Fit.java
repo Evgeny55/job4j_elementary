1295bffa732b9a1da1c8d1fe8458eca699b9c6c6
3.1.Загрузка файлов в gidhub. [#243562] <e.urin1983@gmail.com>
2020-12-23 10:31:51 -0600

    3.1.Загрузка файлов в Gidhub. [#243568]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double rsl = (171 - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (160 - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short heigth = 171;
+        short height = 160;
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Man 171 is " + man);
+        System.out.println("Woman 160 is " + woman);
+    }
+}
