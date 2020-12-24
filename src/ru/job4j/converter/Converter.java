d511496cf58e5b5398a1dd15ba147f873d0baf63
3.1.Загрузка файлов в gidhub. [#243562] <e.urin1983@gmail.com>
2020-12-24 10:07:57 -0600

    3.1.Загрузка файлов в Gidhub. [#243567]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,16 +2,11 @@ package ru.job4j.converter;
 
 public class Converter {
     public static int rubleToEuro(int valve) {
-
         return valve / 70;
     }
-
     public static int rubleToDollars(int valve) {
-
         return valve / 60;
-
     }
-
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         int dollars = Converter.rubleToDollars(120);
