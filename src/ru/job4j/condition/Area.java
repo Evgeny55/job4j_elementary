ae71973999c791593e03eeaa23ec008a6e5deb7d
3.1.Загрузка файлов в gidhub. [#243562] <e.urin1983@gmail.com>
2020-12-26 10:16:10 -0600

    3.1.Загрузка файлов в Gidhub. [#243576]

---
+++ src/ru/job4j/condition/Area.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Area {
+    public static double area(double a, double b, double c) {
+        double x = Math.sqrt(a * b * c);
+        return x;
+    }
+
+    public static void main(String[] args) {
+        double x = Area.area(2,2,2);
+        System.out.println("area (2, 2, 2) = " + x);
+    }
+}
