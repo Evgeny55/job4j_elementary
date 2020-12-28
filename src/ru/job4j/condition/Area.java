e8f0557e3931ede14d1217379fcc86a59f2dc1af
3.1.Загрузка файлов в gidhub. [#243562] <e.urin1983@gmail.com>
2020-12-28 10:13:22 -0600

    3.1.Загрузка файлов в Gidhub. [#243578]

--- src/ru/job4j/condition/Area.java
+++ src/ru/job4j/condition/Area.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Area {
     @SuppressWarnings("checkstyle:ParenPad")
     public static double area(double a, double b, double c) {
-        double p = (a + b + c) / 2.0;
+        double p = (a + b + c) / 2;
         double x = Math.sqrt(p * (p - a ) * (p - b) * (p - c));
         return x;
     }
