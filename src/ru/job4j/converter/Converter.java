b23673617233727b24354a2a5517b8e9eec43918
3.1.Загрузка файлов в gidhub. [#243562] <e.urin1983@gmail.com>
2020-12-28 11:48:46 -0600

    3.1.Загрузка файлов в Gidhub. [#243579]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -15,5 +15,15 @@ public class Converter {
         int dollars = Converter.rubleToDollars(120);
         System.out.println("140 ruble are " + euro + " euro");
         System.out.println("120 ruble are " + dollars + " dollars");
+        int valve1 = 140;
+        int valve2 = 120;
+        int expected1 = 2;
+        int expected2 = 2;
+        int out1 = Converter.rubleToEuro(valve1);
+        int out2 = Converter.rubleToDollars(valve2);
+        boolean passed1 = expected1 == out1;
+        boolean passed2 = expected2 == out2;
+        System.out.println("140 ruble are 1. Test result : " + passed1);
+        System.out.println("120 ruble are 2. Test result : " + passed2);
     }
 }
