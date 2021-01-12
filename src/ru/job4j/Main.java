931f1785a8b4c3f95b87e1f7c42b28dacc3d6b03
Calculator.java [#243562] <e.urin1983@gmail.com>
2021-01-12 10:46:19 -0600

    Calculator.java [#243565]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,10 +1,10 @@
-package ru.job4j;
-
-public class Main {
-    private static java.lang.Object System;
-
-    public static <String> void main(String[] args) {
-
-        System.out.println("Hello world");
-    }
-}
+//package ru.job4j;
+//
+//public class Main {
+//    private static java.lang.Object System;
+//
+//    public static <String> void main(String[] args) {
+//
+//        System.out.println("Hello world");
+//    }
+//}
--- src/ru/job4j/condition/Area.java
+++ src/ru/job4j/condition/Area.java
@@ -1,19 +1,19 @@
-package ru.job4j.condition;
-
-public class Area {
-    private static java.lang.Object Math;
-    private static java.lang.Object System;
-
-    @SuppressWarnings("checkstyle:ParenPad")
-    public static double area(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        double x = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return x;
-    }
-
-    @SuppressWarnings("checkstyle:WhitespaceAfter")
-    public static <String> void main(String[] args) {
-        double x = Area.area(2, 2, 2);
-        System.out.println("area (2, 2, 2) = " + x);
-    }
-}
+//package ru.job4j.condition;
+//
+//public class Area {
+//    private static java.lang.Object Math;
+//    private static java.lang.Object System;
+//
+//    @SuppressWarnings("checkstyle:ParenPad")
+//    public static double area(double a, double b, double c) {
+//        double p = (a + b + c) / 2;
+//        double x = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+//        return x;
+//    }
+//
+//    @SuppressWarnings("checkstyle:WhitespaceAfter")
+//    public static <String> void main(String[] args) {
+//        double x = Area.area(2, 2, 2);
+//        System.out.println("area (2, 2, 2) = " + x);
+//    }
+//}
