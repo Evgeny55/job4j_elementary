bf7ecf2c0f2fc5ef969dd00416b72065d3a5badb
Calculator.java [#243562] <e.urin1983@gmail.com>
2021-01-12 10:04:48 -0600

    Calculator.java [#243564]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+        System.out.println(result);
+
+        int first1 = 5;
+        int second1 = 7;
+
+        boolean result1 = first1 < second1;
+        System.out.println(result1);
+
+        int first2 = 5;
+        int second2 = 5;
+
+        boolean result2 = first2 == second2;
+        System.out.println(result2);
+
+
+    }
+}
