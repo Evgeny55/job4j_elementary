1429915a72fafe537eaee659771e541b817f0c6f
Calculator.java [#243562] <e.urin1983@gmail.com>
2021-01-28 12:32:32 -0600

    Calculator.java [#243578]

---
+++ src/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public static int max(int first, int second, int third) {
+        int rsl = first;
+        if (first > second) {
+        } else if (second > third) {
+            return rsl;
+        }
+    }
+}
