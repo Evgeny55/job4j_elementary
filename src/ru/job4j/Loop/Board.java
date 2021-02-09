4687e990842078ad7c8149f19dd67c809161afa8
Calculator.java [#243562] <e.urin1983@gmail.com>
2021-02-08 23:34:13 -0600

    Calculator.java [#243587]

---
+++ src/ru/job4j/Loop/Board.java
@@ -0,0 +1,25 @@
+package ru.job4j.Loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        //width ширина, height высота
+        for (int row = 0; row < width; row++) {
+            for (int cell = 0; cell < height; cell++) {
+                if ((row + cell) % 2 == 0) {
+                    System.out.println("X");
+                } else {
+                    System.out.println("X");
+                }
+            }
+
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+    }
+}
