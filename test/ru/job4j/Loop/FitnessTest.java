758d31acdbc3d5b24f6faef0abbed53bb919e40f
Calculator.java [#243562] <e.urin1983@gmail.com>
2021-02-15 10:41:31 -0600

    Calculator.java [#243590]

--- test/ru/job4j/Loop/FitnessTest.java
+++ test/ru/job4j/Loop/FitnessTest.java
@@ -1,26 +1,27 @@
-//package ru.job4j.Loop;
-//
-//import org.junit.Test;
-//
-//import static org.hamcrest.Matchers.is;
-//import static org.junit.Assert.*;
-//
-//public class FitnessTest {
-//    @Test
-//    public void whenIvanGreatNik() {
-//        int month = Factorial.calc(int 95, int 90);
-//        assertThat(month, is(0));
-//    }
-//
-//    @Test
-//    public void whenIvanLessByOneNik() {
-//        int month = Factorial.calc(int 90, int 95);
-//        assertThat(month, is(1));
-//    }
-//
-//    @Test
-//    public void whenIvanLessByFewNik() {
-//        int month = Factorial.calc(int 50, int 90);
-//        assertThat(month, is(2));
-//    }
-//}
+package ru.job4j.loop;
+
+import org.junit.Test;
+import ru.job4j.Loop.Fitness;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+    @Test
+    public void whenIvanGreatNik() {
+        int month = Fitness.calc(95, 90);
+        assertThat(month, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        int month = Fitness.calc(90, 95);
+        assertThat(month, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        int month = Fitness.calc(50, 90);
+        assertThat(month, is(2));
+    }
+}
